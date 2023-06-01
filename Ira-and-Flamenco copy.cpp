d030d7c992e2d9c49b65654206685860f3efee83
Anirudh Kaushik <anirudh.ka@research.iiit.ac.in>
2023-06-01 19:15:27 +0530

    1700 rated solution added

--- Ira-and-Flamenco copy.cpp
+++ Ira-and-Flamenco copy.cpp
@@ -8,19 +8,6 @@ using namespace std;
 #define pll pair<ll, ll>
 #define pii pair<int, int>
 #define vi vector<int>
-int cond = 0;
-
-long long binpow(long long a, long long b, long long m) {
-    a %= m;
-    long long res = 1;
-    while (b > 0) {
-        if (b & 1)
-            res = res * a % m;
-        a = a * a % m;
-        b >>= 1;
-    }
-    return res;
-}
 
 void solve()
 {
@@ -67,15 +54,25 @@ void solve()
     vll dp(s.size(), -1);
     int j = s.size() - 1;
 
-    for (int i = j - m + 1; i >= 0 && j >= m - 1;)
+    vll abc(n+1, -1);
+    for (int i = s.size() - 2; i >= 0 && j >= 0;)
     {
-        i = j - m + 1;
 
         if ((s[j] - s[i] < m))
         {
-            dp[i] = j;
+            if (abs(j - i + 1) >= m)
+            {
+                dp[i] = j;
+            }
+
+            i--;
         }
-        j--;
+        else
+        {
+            j--;
+        }
+        while (j <= i)
+            i--;
     }
     // for(auto it: dp)
     // {
@@ -83,49 +80,37 @@ void solve()
     // }
     // cout<<endl;
     ll ans = 0;
-    vll prefix_product(s.size(), 1);
-    prefix_product[0] *= hasmap[s[0]];
-    prefix_product[0] %= p;
-    // cout << prefix_product[0] << " ";
-    for (int i = 1; i < s.size(); i++)
-    {
-        prefix_product[i] *= hasmap[s[i]];
-        prefix_product[i] %= p;
-        prefix_product[i] *= prefix_product[i - 1];
-        prefix_product[i] %= p;
-        // cout << prefix_product[i] << " ";
-    }
-    // cout << endl;
     for (int i = 0; i < s.size(); i++)
     {
         if (dp[i] != -1)
         {
             ll range = dp[i] - i + 1;
             // pow_n should be calculated using binary exponentiation, it is equal to 2^range modulo 1e9+7
-            // cout << "range: " << range << " " << i << " " << dp[i] <<  endl;
+            // cout << "range: " << range << endl;
             ll pow_n = 1;
-            if (i > 0)
+            if (abc[range] != -1)
             {
-                pow_n = prefix_product[dp[i]]%p;
-                pow_n *= binpow(prefix_product[i - 1], p - 2, p); // dont divide by prefix, instead multiply by inverse modulo IMPORTANT as this kept failing test cases
+                pow_n = abc[range];
             }
             else
             {
-                pow_n = prefix_product[dp[i]];
+                for (int i = 1; i <= m; i++)
+                {
+                    pow_n = pow_n * (range - m + i) / i;
+                    pow_n %= p;
+                    abc[range] = pow_n;
+                }
             }
-            // cout << "pow_n: " << pow_n << endl;
-            // pow_n %= p;
-
             // cout << "ncr: " << pow_n << endl;
-            // for (int l = i; l <= dp[i]; l++)
-            // {
-            //     if (hasmap[s[l]] > 1)
-            //     {
-            //         pow_n *= hasmap[s[l]];
-            //         // cout << "hasmap: " << hasmap[s[l]] << endl;
-            //         pow_n %= p;
-            //     }
-            // }
+            for (int l = i; l <= dp[i]; l++)
+            {
+                if (hasmap[s[l]] > 1)
+                {
+                    pow_n *= hasmap[s[l]];
+                    // cout << "hasmap: " << hasmap[s[l]] << endl;
+                    pow_n %= p;
+                }
+            }
 
             pow_n %= p;
             ans += pow_n;
@@ -142,10 +127,6 @@ int main()
     cout.tie(NULL);
     int t;
     cin >> t;
-    if (t == 100)
-        cond = 1;
-    else
-        cond = 0;
     while (t--)
     {
         solve();
