d18f171f8508464e2ad4f986086eda9a38d11273
Anirudh Kaushik <anirudhkaushik2003@gmail.com>
2023-08-12 14:29:34 +0530

    100

---
+++ Longest-Divisors-Interval.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vll vector<ll>
+#define pb push_back
+#define mp make_pair
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define vi vector<int>
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll i;
+    ll count = 0;
+    for( i= 1; i<= n;i++)
+    {
+        if(n%i != 0)
+        {
+            break;
+        }
+        else{
+            count++;
+        }
+    }
+    cout << count << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
